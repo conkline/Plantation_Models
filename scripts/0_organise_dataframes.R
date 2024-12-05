5cf2cb14ec5a76cbc3b7c8dfb290f10ff9172e6d
gcerullo <grcerullo@gmail.com>
2024-12-05 14:09:43 -0800

    tidy dfs

--- scripts/0_organise_dataframes.R
+++ scripts/0_organise_dataframes.R
@@ -5,9 +5,12 @@ library(tidyverse)
 #read in data (assumed you have data stored in a folder called 'inputs' in your r project)
 df <- read.csv("inputs/bukoski_params.csv")
 
-# Remove spaces and colons from all columns
 df <- df %>%
+  # Remove spaces and colons from all columns
   mutate(across(everything(), ~ str_replace_all(.x, "[ :]", ""))) %>%  
-  pivot_wider(names_from = parametre, values_from = value)
+  pivot_wider(names_from = parametre, values_from = value) %>%  
+  #make all data numeric
+  mutate(across(everything(), ~ as.numeric(str_remove(.x, "[^0-9.]+"))))
 
-write_csv
+#save clean df output 
+write_csv(df, "inputs/cleandf_params.csv")
